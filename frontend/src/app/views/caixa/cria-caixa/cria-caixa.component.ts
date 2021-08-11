cfdd1cd3a33525226c7ed6b6aa64ae63279e003d
Kaique Motta <kaique.motta@hotmail.com>
2021-08-11 19:03:33 -0300

    melhoria nav

---
+++ frontend/src/app/views/caixa/cria-caixa/cria-caixa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cria-caixa',
+  templateUrl: './cria-caixa.component.html',
+  styleUrls: ['./cria-caixa.component.css']
+})
+export class CriaCaixaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
